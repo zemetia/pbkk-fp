cc487aab5dc05e899968d5ab601fc72f848de523
Revanantyo D <xmall75.id@gmail.com>
2023-11-02 19:41:21 +0700

    fix route web.php

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
+// Route::get('/login', function () {
+//     return view('login');
+// });
 
-Route::get('/register', function () {
-    return view('register');
-});
+// Route::get('/register', function () {
+//     return view('register');
+// });
